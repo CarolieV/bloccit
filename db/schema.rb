9c5e88d94284167b1e9b5eb1364e1b49454573e3
Carolie Ventola <carolie.ventola98@gmail.com>
2018-01-27 19:24:36 +0000

    Authentication- User Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180103233958) do
+ActiveRecord::Schema.define(version: 20180127185255) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -52,6 +52,14 @@ ActiveRecord::Schema.define(version: 20180103233958) do
     t.boolean  "resolved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
